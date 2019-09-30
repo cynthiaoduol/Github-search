2c5d8ecf011c10c301874c1de84f52e401cb1e25
cynthiaoduol <cynthiaobu940@gmail.com>
2019-09-30 10:19:45 +0300

    add a function to get the user repositories

--- src/app/profile-service/profile.service.ts
+++ src/app/profile-service/profile.service.ts
@@ -21,5 +21,10 @@ export class ProfileService {
      return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
      .pipe(map((response: any) => response)); 
    }
+
+   getRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+    .pipe(map((response: any) => response)); 
+   }
    
 }
