22833064403fb22924db275798668385767a57a6
cynthiaoduol <cynthiaobu940@gmail.com>
2019-09-30 17:07:10 +0300

    add an empty path with the redirectTo propery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
+import { NotFoundComponent } from './not-found/not-found.component';
 import { AboutComponent } from './about/about.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
+
 const routes: Routes = [
   { path: 'profile', component: ProfileComponent},
   { path: 'about', component: AboutComponent},
+  { path: '**', component:NotFoundComponent},
+
+  { path: '', redirectTo:"/goals", pathMatch:"full"},
 ];
 
 @NgModule({
