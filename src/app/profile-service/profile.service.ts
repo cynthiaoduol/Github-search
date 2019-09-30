85ab8e81174b4065d2dafff8899924440c7585dc
cynthiaoduol <cynthiaobu940@gmail.com>
2019-09-30 12:02:52 +0300

    update profile username

--- src/app/profile-service/profile.service.ts
+++ src/app/profile-service/profile.service.ts
@@ -26,5 +26,9 @@ export class ProfileService {
     return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
     .pipe(map((response: any) => response)); 
    }
+
+   updateProfile(username:string){
+    this.username = username;
+   }
    
 }
